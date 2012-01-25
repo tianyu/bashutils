42f7fd4bca2dd62648ae6f6eef3b4d368ead20d6
Tianyu Zhu <tian.tian098@gmail.com>
2012-01-25 00:51:18 -0500

    Added splitln.c (UNTESTED) (UNCOMPILED)

---
+++ src/splitln.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void endLine() {
+    # TODO
+}
+
+int main() {
+    for (int c = getchar(); c != EOF; c = getchar()) {
+        switch (c) {
+        case EOF:
+            goto end;
+        case '.': case '!': case '?':
+            endLine();
+            break;
+        case '\n':
+            putchar('\r');
+            putchar('\n');
+            break;
+        default:
+            putchar(c);
+            break;
+        }
+    } end:
+    return 0;
+}
