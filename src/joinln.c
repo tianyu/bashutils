fc3d7f90890aed4b6417da7e324f57af6b3a90ab
Tianyu Zhu <tian.tian098@gmail.com>
2012-01-30 10:48:39 -0500

    Implemented joinLine

--- src/joinln.c
+++ src/joinln.c
@@ -1,4 +1,24 @@
 #include "joinln.h"
+#include <stdio.h>
 
 void joinLine() {
+    int c;
+    for (c = getchar(); c != EOF; c = getchar()) {
+        if (c == '\n') {
+            putchar(' ');
+        } else if (c == '\r') {
+            c = getchar();
+            if (c == EOF) {
+                putchar('\r');
+                break;
+            } else if (c == '\n') {
+                putchar('\n');
+            } else {
+                putchar('\r');
+                putchar(c);
+            }
+        } else {
+            putchar(c);
+        }
+    }
 }
